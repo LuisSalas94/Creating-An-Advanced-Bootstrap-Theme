7857fa08993fb1e213542b4d78e33e334defca5a
Fernando Salas <luisfernandosalasg@gmail.com>
2021-03-10 21:20:25 -0500

    Finish Web Page

--- Nuno Theme/js/custom.js
+++ Nuno Theme/js/custom.js
@@ -46,3 +46,63 @@ $(document).ready(function () {
 		$(".arrow").css("opacity", 1 - $(window).scrollTop() / 250);
 	});
 });
+
+/*============Meet the team=============*/
+$(document).ready(function () {
+	$("#team-slider").owlCarousel({
+		items: 3,
+		autoplay: true,
+		smartSpeed: 700,
+		loop: true,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			576: {
+				items: 2,
+			},
+			768: {
+				items: 3,
+			},
+		},
+	});
+});
+
+/*============Skills Counter=============*/
+$(document).ready(function () {
+	$(".counter").counterUp({
+		delay: 10,
+		time: 1900,
+	});
+});
+
+/*============Clients Carousel=============*/
+$(document).ready(function () {
+	$("#clients-slider").owlCarousel({
+		items: 2,
+		autoplay: true,
+		smartSpeed: 1700,
+		loop: true,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			768: {
+				items: 2,
+			},
+		},
+	});
+});
+
+/*============Top Scroll============*/
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 500) {
+			$(".top-scroll").fadeIn();
+		} else {
+			$(".top-scroll").fadeOut();
+		}
+	});
+});
